161878a24b0dc2bd03a9f0147228584755ae5e3c
Duncan Roosma <droosma@xpirit.com>
2021-02-17 20:03:10 +0100

    Add nuget information and readme.md

---
+++ src/Resilient.HttpClient/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Resilient.HttpClient
+{
+    public class Class1
+    {
+        public Class1()
+        {
+            var settings = new ResilienceSettings(new ResilienceSettings.CircuitBreakerSettings(3, onBreak: (exception, duration) => Console.WriteLine($"circuit open for {duration:g}: {exception.Message}")),
+                                                  new ResilienceSettings.RetrySettings(3));
+        }
+    }
+}
--- src/Resilient.HttpClient/ResilienceSettings.cs
+++ src/Resilient.HttpClient/ResilienceSettings.cs
@@ -8,56 +8,92 @@ namespace Resilient.HttpClient
 {
     public class ResilienceSettings
     {
-        public CircuitBreakerSettings? CircuitBreaker { get; }
-        public RetrySettings? Retry { get; }
-
-        public ResilienceSettings(CircuitBreakerSettings? circuitBreaker = null, 
+        public ResilienceSettings(CircuitBreakerSettings? circuitBreaker = null,
                                   RetrySettings? retry = null)
         {
             CircuitBreaker = circuitBreaker;
             Retry = retry;
         }
 
+        public CircuitBreakerSettings? CircuitBreaker { get; }
+        public RetrySettings? Retry { get; }
+
         public class CircuitBreakerSettings
         {
             public CircuitBreakerSettings(int circuitBreakerAllowedAttempts,
-                                          TimeSpan circuitBreakerDuration,
+                                          TimeSpan? circuitBreakerDuration = null,
                                           Action<Exception, TimeSpan>? onBreak = null,
                                           Action? onReset = null,
                                           Action? onHalfOpen = null)
             {
                 CircuitBreakerAllowedAttempts = circuitBreakerAllowedAttempts;
-                CircuitBreakerDuration = circuitBreakerDuration;
+                CircuitBreakerDuration = circuitBreakerDuration ?? TimeSpan.FromSeconds(5);
                 OnBreak = onBreak ?? ((_, _) => { });
-                OnReset = onReset ?? (() => {});
+                OnReset = onReset ?? (() => { });
                 OnHalfOpen = onHalfOpen ?? (() => { });
             }
 
+            /// <summary>
+            /// Number of failures before breaking
+            /// </summary>
             public int CircuitBreakerAllowedAttempts { get; }
+
+            /// <summary>
+            /// Time the circuit breaks for
+            /// </summary>
             public TimeSpan CircuitBreakerDuration { get; }
+
+            /// <summary>
+            /// Hook that triggers on trigger of circuit breaker
+            /// </summary>
             public Action<Exception, TimeSpan>? OnBreak { get; }
+
+            /// <summary>
+            /// Hook that triggers on reset of circuit breaker
+            /// </summary>
             public Action? OnReset { get; }
+
+            /// <summary>
+            /// Hook that triggers on half open circuit breaker
+            /// </summary>
             public Action? OnHalfOpen { get; }
         }
 
         public class RetrySettings
         {
             public RetrySettings(int retryCount,
-                                 TimeSpan retrySeedDelay,
-                                 TimeSpan retryDelay,
+                                 TimeSpan? retrySeedDelay = null,
+                                 TimeSpan? retryDelay = null,
                                  Func<DelegateResult<HttpResponseMessage>, TimeSpan, int, Context, Task>? onRetryAsync = null)
             {
                 RetryCount = retryCount;
-                RetrySeedDelay = retrySeedDelay;
-                RetryDelay = retryDelay;
+                RetrySeedDelay = retrySeedDelay ?? TimeSpan.FromSeconds(5);
+                RetryDelay = retryDelay ?? TimeSpan.FromSeconds(10);
 
-                OnRetryAsync = onRetryAsync ?? ((_, _, _, _) => Task.CompletedTask);
+                OnRetryAsync = onRetryAsync ?? ((_,
+                                                 _,
+                                                 _,
+                                                 _) => Task.CompletedTask);
             }
 
+            /// <summary>
+            /// Number of retries before breaking
+            /// </summary>
             public int RetryCount { get; }
+
+            /// <summary>
+            /// Seed used for creating retry delay, a jitter is added to this
+            /// </summary>
             public TimeSpan RetrySeedDelay { get; }
+
+            /// <summary>
+            /// Maximum time between retries
+            /// </summary>
             public TimeSpan RetryDelay { get; }
 
+            /// <summary>
+            /// Hook for 
+            /// </summary>
             public Func<DelegateResult<HttpResponseMessage>, TimeSpan, int, Context, Task> OnRetryAsync { get; }
         }
     }
